eae7dd50f99f43305cb241fea43ed63922821a25
Thomas Klein <132069614+ThomSMG@users.noreply.github.com>
2024-06-13 14:17:38 +0200

    Chore/dependencies eslint updates (#110)
    * fix dependency versions and add newly required @testing-library/dom
    
    * fix eslint issues
    
    ---------
    
    Co-authored-by: Thomas Klein <thomas.klein@toptal.com>

--- src/components/Listing/Listing.test.tsx
+++ src/components/Listing/Listing.test.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 
 import Listing from ".";
@@ -9,12 +8,8 @@ describe("<Listing />", () => {
     title: "A new listing",
   };
 
-  beforeEach(() => {
-    // eslint-disable-next-line testing-library/no-render-in-setup
-    render(<Listing {...defaultProps} />);
-  });
-
   it("should render", () => {
+    render(<Listing {...defaultProps} />);
     // This is a basic check to ensure the component is rendered. 
     // For more specific checks, you can query elements inside the component.
     expect(screen.getByText('A new listing')).toBeTruthy();
