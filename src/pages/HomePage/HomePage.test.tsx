060a38e976a2664d2cf2c1fb555364ba450e90e7
Thomas Klein <thomas.klein@swissmarketplace.group>
2025-05-11 23:22:29 +0700

    fix(test): wrap HomePage in MemoryRouter for testing

--- src/pages/HomePage/HomePage.test.tsx
+++ src/pages/HomePage/HomePage.test.tsx
@@ -1,10 +1,15 @@
 import { render } from "@testing-library/react";
 import '@testing-library/jest-dom'
+import { MemoryRouter } from 'react-router-dom';
 import HomePage from ".";
 
 describe("<HomePage />", () => {
   it("should render", () => {
-    const { container } = render(<HomePage />);
+    const { container } = render(
+      <MemoryRouter>
+        <HomePage />
+      </MemoryRouter>
+    );
     expect(container).toBeTruthy();
   });
 });
